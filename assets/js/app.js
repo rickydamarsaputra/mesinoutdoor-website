7c1e713aa852b57a87cdf91e37be9b328dc6f88b
rickydamarsaputra <retrocode.rc@gmail.com>
2025-01-17 10:41:03 +0700

    update: class for gtgm

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,35 @@
-import { createApp, ref, onMounted } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
 createApp({
   setup() {
-    const products = ref([]);
+    const products = ref([
+      {
+        id: 1,
+        name: 'NEU Jet MPOC4K',
+        tagline: 'Cetak Tanpa Batas dengan Harga Terjangkau.',
+        photo: './assets/images/mesin/neu-jet-mpoc4k.webp',
+        short_description: 'Kami hadirkan Mesin Printer NEU Jet MPOC4K, printer profesional dengan 4 printhead Piezo-Electric tipe 512i (30PL) yang dirancang untuk hasil cetak berkualitas tinggi. Mesin ini mendukung kecepatan hingga 120 m² pada 2 pass dan dilengkapi dengan fitur canggih seperti sistem suction yang dapat disesuaikan, sistem anti-collision, serta lampu LED pada area cetak. Dengan daya 4000 watt dan desain Roll-to-Roll, printer ini sangat cocok untuk kebutuhan pencetakan skala besar yang presisi dan efisien.',
+        link_product: 'https://vicentra.co.id/product/mesin/digital-printing/outdoor/neu-jet-mpoc4k',
+      },
+      {
+        id: 2,
+        name: 'NEU Jet MPOC8K',
+        tagline: 'Kecepatan dan Ketepatan dalam Setiap Cetak.',
+        photo: './assets/images/mesin/neu-jet-mpoc8k.webp',
+        short_description: 'Mesin NEU Jet MPOC8K adalah mesin cetak dengan empat printhead KM 512i (30PL) yang dapat ditingkatkan hingga delapan printhead. Mesin ini menggunakan perangkat lunak RIP Photoprint dan mendukung media handling roll-to-roll. Mesin ini memiliki kecepatan cetak yang bervariasi berdasarkan jumlah pass, dengan lebar area cetak mencapai 320 cm. Mesin ini dilengkapi dengan berbagai sistem canggih seperti sistem posisi carriage 180 dpi, sistem perlindungan printhead, dan sistem pengeringan gambar menggunakan pemanas inframerah.',
+        link_product: 'https://vicentra.co.id/product/mesin/digital-printing/outdoor/neu-jet-mpoc8k',
+      }
+    ]);
+    const isMenuShow = ref(false);
 
-    onMounted(() => {
-      fetch('https://vicentra.co.id/api/products/outdoor')
-        .then(res => res.json())
-        .then((data) => {
-          products.value = data.data;
-        });
-    });
+    const toggleMenu = () => {
+      isMenuShow.value = !isMenuShow.value;
+    }
 
     return {
-      products
+      products,
+      isMenuShow,
+      toggleMenu
     }
   }
 }).mount('#app')
