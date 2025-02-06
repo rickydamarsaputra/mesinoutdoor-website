4d4ad180ab3c64dc9d15052bed39ad2e88615b31
rickydamarsaputra <retrocode.rc@gmail.com>
2025-02-06 10:33:25 +0700

    update: ubah ke ssr php

---
+++ const.php
@@ -0,0 +1,54 @@
+<?php
+$products = [
+  [
+    "id" => 1,
+    "name" => "NEU Jet MPOC4K",
+    "tagline" => "Cetak Tanpa Batas dengan Harga Terjangkau.",
+    "photo" => "./assets/images/mesin/neu-jet-mpoc4k.webp",
+    "short_description" => "Kami hadirkan Mesin Printer NEU Jet MPOC4K, printer profesional dengan 4 printhead Piezo-Electric tipe 512i (30PL) yang dirancang untuk hasil cetak berkualitas tinggi. Mesin ini mendukung kecepatan hingga 120 m² pada 2 pass dan dilengkapi dengan fitur canggih seperti sistem suction yang dapat disesuaikan, sistem anti-collision, serta lampu LED pada area cetak. Dengan daya 4000 watt dan desain Roll-to-Roll, printer ini sangat cocok untuk kebutuhan pencetakan skala besar yang presisi dan efisien.",
+    "link_product" => "https://vicentra.co.id/product/mesin/digital-printing/outdoor/neu-jet-mpoc4k",
+  ],
+  [
+    "id" => 2,
+    "name" => "NEU Jet MPOC8K",
+    "tagline" => "Kecepatan dan Ketepatan dalam Setiap Cetak.",
+    "photo" => "./assets/images/mesin/neu-jet-mpoc8k.webp",
+    "short_description" => "Mesin NEU Jet MPOC8K adalah mesin cetak dengan empat printhead KM 512i (30PL) yang dapat ditingkatkan hingga delapan printhead. Mesin ini menggunakan perangkat lunak RIP Photoprint dan mendukung media handling roll-to-roll. Mesin ini memiliki kecepatan cetak yang bervariasi berdasarkan jumlah pass, dengan lebar area cetak mencapai 320 cm. Mesin ini dilengkapi dengan berbagai sistem canggih seperti sistem posisi carriage 180 dpi, sistem perlindungan printhead, dan sistem pengeringan gambar menggunakan pemanas inframerah.",
+    "link_product" => "https://vicentra.co.id/product/mesin/digital-printing/outdoor/neu-jet-mpoc8k",
+  ]
+];
+
+$salesPeople = [
+  [
+    "id" => 1,
+    "name" => "Layanan 24 Jam",
+    "slug" => "layanan_24_jam",
+    "phone" => "6281232548368",
+    "photo" => "https://vicentra.co.id/storage/sales-people/vicentra-24-jam.webp",
+    "additional_sentence" => "Ada pertanyaan atau ingin memesan produk ini?"
+  ],
+  // [
+  //     "id" => 2,
+  //     "name" => "Yaya",
+  //     "slug" => "yaya",
+  //     "phone" => "6285175005535",
+  //     "photo" => "https://vicentra.co.id/storage/sales-people/sales-yaya.webp",
+  //     "additional_sentence" => "Support 24 jam"
+  // ],
+  // [
+  //     "id" => 3,
+  //     "name" => "Alda",
+  //     "slug" => "alda",
+  //     "phone" => "6285183171200",
+  //     "photo" => "https://vicentra.co.id/storage/sales-people/sales-alda.webp",
+  //     "additional_sentence" => "Ada pertanyaan atau ingin memesan produk ini?"
+  // ],
+  // [
+  //     "id" => 4,
+  //     "name" => "Ella",
+  //     "slug" => "ella",
+  //     "phone" => "6285733399974",
+  //     "photo" => "https://vicentra.co.id/storage/sales-people/sales-ella.webp",
+  //     "additional_sentence" => "Ada pertanyaan atau ingin memesan produk ini?"
+  // ],
+];
---
+++ index.php
@@ -0,0 +1,205 @@
+<?php
+require_once('const.php');
+?>
+<!doctype html>
+<html lang="id">
+
+<head>
+  <!-- Metas -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <meta name="author" content="Vicentra Dev" />
+  <meta name="description" content="Vicentra - Spesialis mesin digital printing outdoor dengan harga terbaik. berbagai merek unggulan, serta layanan purna jual terpercaya. Dapatkan solusi cetak berkualitas tinggi untuk kebutuhan bisnis Anda. Hubungi kami sekarang!" />
+  <meta name="keywords" content="distributor mesin digital printing, harga mesin digital printing outdoor, harga mesin digital printing outdoor murah, harga mesin digital printing outdoor rekondisi, harga mesin digital printing outdoor second, harga mesin digital printing outdoor terbaik, harga mesin outdoor digital printing, jual mesin digital printing, merk mesin digital printing outdoor terbaik, mesin digital printing outdoor, mesin digital printing outdoor ac, mesin digital printing outdoor ac surabaya, mesin digital printing outdoor adalah, mesin digital printing outdoor atlas g4 km512i, mesin digital printing outdoor atlas konica st km512i 30 pl, mesin digital printing outdoor bali, mesin digital printing outdoor baru, mesin digital printing outdoor bekas, mesin digital printing outdoor bekas surabaya, mesin digital printing outdoor berapa, mesin digital printing outdoor berapa harganya, mesin digital printing outdoor color, mesin digital printing outdoor eco solvent, mesin digital printing outdoor epson, mesin digital printing outdoor epson surabaya, mesin digital printing outdoor error, mesin digital printing outdoor eye color, mesin digital printing outdoor flora, mesin digital printing outdoor food, mesin digital printing outdoor foto, mesin digital printing outdoor free, mesin digital printing outdoor free download, mesin digital printing outdoor furniture, mesin digital printing outdoor inno kv 3204, mesin digital printing outdoor jakarta, mesin digital printing outdoor jatim, mesin digital printing outdoor jawa timur, mesin digital printing outdoor jual, mesin digital printing outdoor konica st km512i 30 pl, mesin digital printing outdoor lab, mesin digital printing outdoor lamongan, mesin digital printing outdoor lampu, mesin digital printing outdoor laser, mesin digital printing outdoor murah, mesin digital printing outdoor namanya apa, mesin digital printing outdoor ngagel, mesin digital printing outdoor nganjuk, mesin digital printing outdoor offset, mesin digital printing outdoor olx, mesin digital printing outdoor original, mesin digital printing outdoor otomatis, mesin digital printing outdoor outdoor, mesin digital printing outdoor outdoor surabaya, mesin digital printing outdoor png, mesin digital printing outdoor qc, mesin digital printing outdoor quality, mesin digital printing outdoor quantum, mesin digital printing outdoor quantum surabaya, mesin digital printing outdoor quora, mesin digital printing outdoor quotes, mesin digital printing outdoor rekondisi, mesin digital printing outdoor second, mesin digital printing outdoor terbaik, mesin digital printing outdoor ukuran, mesin digital printing outdoor ukuran berapa, mesin digital printing outdoor unit, mesin digital printing outdoor unit surabaya, mesin digital printing outdoor untuk apa, mesin digital printing outdoor uv, mesin digital printing outdoor valve, mesin digital printing outdoor vector, mesin digital printing outdoor video, mesin digital printing outdoor warehouse, mesin digital printing outdoor warna hitam, mesin digital printing outdoor wifi, mesin digital printing outdoor wilayah surabaya, mesin digital printing outdoor xiaomi, mesin digital printing outdoor xl, mesin digital printing outdoor xp600, mesin digital printing outdoor xpe 1600 ecosolvent, mesin digital printing outdoor xuli, mesin digital printing outdoor yang aman, mesin digital printing outdoor yang bagus, mesin digital printing outdoor yang bagus merk apa, mesin digital printing outdoor yogyakarta, mesin digital printing outdoor zebra, mesin digital printing outdoor zebra surabaya, mesin digital printing outdoor zx, mesin outdoor digital printing, pengertian mesin digital printing outdoor, perbedaan mesin digital printing indoor dan outdoor, perbedaan mesin digital printing outdoor dan indoor, spesifikasi mesin digital printing outdoor">
+  <meta name="robots" content="index, follow, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
+
+  <title>Vicentra - MESIN OUTDOOR</title>
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Vicentra - Mesin Outdoor" />
+  <meta property="og:description"
+    content="Vicentra - Spesialis mesin digital printing outdoor dengan harga terbaik. berbagai merek unggulan, serta layanan purna jual terpercaya. Dapatkan solusi cetak berkualitas tinggi untuk kebutuhan bisnis Anda. Hubungi kami sekarang!" />
+  <meta property="og:image" content="https://mesinoutdoor.com/assets/images/logo-vicentra-black.webp" />
+  <meta property="og:url" content="https://mesinoutdoor.com" />
+  <meta property="og:site_name" content="Vicentra - Mesin Outdoor" />
+
+  <!-- Twitter -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="Vicentra - Mesin Outdoor" />
+  <meta name="twitter:description"
+    content="Vicentra - Spesialis mesin digital printing outdoor dengan harga terbaik. berbagai merek unggulan, serta layanan purna jual terpercaya. Dapatkan solusi cetak berkualitas tinggi untuk kebutuhan bisnis Anda. Hubungi kami sekarang!" />
+  <meta name="twitter:image" content="https://mesinoutdoor.com/assets/images/logo-vicentra-black.webp" />
+  <meta name="twitter:site" content="https://mesinoutdoor.com" />
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdn.tailwindcss.com"></script>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-MFJX7SQF');
+  </script>
+  <!-- End Google Tag Manager -->
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-RTZXL9G14K"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-RTZXL9G14K');
+  </script>
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MFJX7SQF" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <div id="app">
+    <!-- HEADER -->
+    <header class="border-b-2 py-4">
+      <div class="lg:w-[80vw] mx-5 lg:mx-auto flex justify-between items-center relative">
+        <img src="./assets/images/logo-vicentra-black.webp" alt="logo-vicentra-black" class="h-[2.375rem]">
+        <div class="lg:hidden">
+          <i :class="[isMenuShow ? 'fa-solid fa-xmark text-2xl' : 'fa-solid fa-bars text-2xl']" class=""
+            @click="toggleMenu"></i>
+        </div>
+        <ul :class="[isMenuShow ? 'visible' : 'invisible']"
+          class="lg:visible flex flex-col lg:flex-row lg:items-center gap-2 lg:gap-5 absolute lg:static top-[3.125rem] left-0 right-0 bg-white lg:bg-transparent shadow lg:shadow-none p-2 lg:p-0 rounded lg:rounded-none">
+          <li><a href="https://vicentra.co.id" class="beranda hover:underline">Beranda</a></li>
+          <li><a href="https://vicentra.co.id/product/mesin/digital-printing/outdoor"
+              class="semua_produk hover:underline">Produk</a>
+          </li>
+          <li><a href="https://vicentra.co.id/terms-and-conditions"
+              class="layanan_dan_perbaikan hover:underline">Layanan dan Perbaikan</a>
+          </li>
+          <li><a href="https://vicentra.co.id/about-us" class="tentang_kami hover:underline">Tentang Kami</a></li>
+        </ul>
+      </div>
+    </header>
+    <!-- HEADER -->
+
+    <!-- HERO SECTION -->
+    <section
+      class="lg:w-[80vw] mx-5 lg:mx-auto grid lg:grid-cols-2 gap-5 items-center mx-5 px-5 py-[3.125rem] bg-[#f7f7f7]">
+      <div>
+        <h1 class="text-4xl lg:text-3xl font-semibold text-center lg:text-left">
+          MESIN OUTDOOR TERBAIK, PERIOD
+        </h1>
+        <p class="mt-5">
+          Selamat datang di website kami! Temukan mesin digital printing outdoor berkualitas dengan hasil tahan lama dan
+          harga terbaik di sini.
+        </p>
+        <a href="https://vicentra.co.id/product/mesin/digital-printing/outdoor" target="_blank" class="semua_produk inline-block capitalize mt-5 px-10 py-3 hover:bg-[#1B0000] hover:text-white border border-[#1B0000]
+          transition duration-300">lihat
+          semua produk</a>
+      </div>
+      <div class="order-first lg:order-none">
+        <img src="./assets/images/mesin/neu-jet-mpoc4k.webp" alt="neu-jet-mpoc4k">
+      </div>
+    </section>
+    <!-- HERO SECTION -->
+
+    <!-- PRODUCT SECTION -->
+    <section>
+      <div class="lg:w-[80vw] mx-5 lg:mx-auto">
+        <?php foreach ($products as $product) : ?>
+          <div>
+            <div class="flex justify-center mt-10">
+              <div class="w-[80vw] h-[1px] bg-[#1B0000]"></div>
+            </div>
+            <div class="grid lg:grid-cols-2 gap-5 items-center my-[5rem]">
+              <div>
+                <h2><?= $product['name'] ?></h2>
+                <h1 class="text-3xl font-semibold my-5"><?= $product['tagline'] ?></h1>
+                <div>
+                  <?= $product['short_description'] ?>
+                </div>
+                <a href="<?= $product['link_product'] ?>" target="_blank"
+                  class="detail_produk inline-block capitalize font-medium text-white bg-black rounded-md px-10 py-3 mt-5">lebih
+                  lengkap</a>
+              </div>
+              <div class="order-first lg:order-none">
+                <a href="<?= $product['link_product'] ?>" target="_blank">
+                  <img src="<?= $product['photo'] ?>" alt="<?= $product['name'] ?>">
+                </a>
+              </div>
+            </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </section>
+    <!-- PRODUCT SECTION -->
+
+    <!-- CTA SECTION -->
+    <section class="mb-[2.5rem]">
+      <h1 class="capitalize text-3xl font-semibold text-center">Hubungi Kami</h1>
+      <div class="relative flex justify-center mt-5">
+        <div class="w-[80vw] h-[2px] bg-[#e1e1e1]"></div>
+        <div class="w-[10vw] h-[2px] bg-[#1B0000] absolute"></div>
+      </div>
+      <div class="lg:w-[40vw] mx-5 lg:mx-auto flex justify-center mt-10">
+        <?php foreach ($salesPeople as $sales): ?>
+          <a href="https://api.whatsapp.com/send?phone=<?= $sales['phone']; ?>&text=Hallo%20Vicentra,%20Saya%20Ingin%20Memesan%20Mesin%20UV."
+            target="_blank"
+            class="<?= ($sales['id'] === 1) ? 'bg-[#006db8]' : 'bg-green-600'; ?> sales_<?= $sales['slug']; ?>_cta_link w-full h-[5rem] flex items-center gap-[1.5rem] rounded-full relative py-2 pl-[7rem]">
+
+            <div class="absolute left-0">
+              <div class="w-[5.5rem] h-[5.5rem] rounded-full border-[0.188rem] border-white relative">
+                <img src="<?= $sales['photo']; ?>" alt="<?= $sales['name']; ?>" class="rounded-full" />
+                <div class="w-[1.5rem] h-[1.5rem] flex justify-center items-center bg-white rounded-full absolute right-[-0.75rem] top-[50%] transform translate-y-[-50%]">
+                  <i class="fa-brands fa-whatsapp text-green-500"></i>
+                </div>
+              </div>
+            </div>
+
+            <div class="mr-4">
+              <div class="flex gap-2">
+                <h1 class="text-sm font-medium text-white/50 capitalize">
+                  <?= $sales['name']; ?>
+                </h1>
+                <span class="flex justify-center items-center text-xs font-semibold text-white bg-white/20 px-2 rounded-md capitalize">
+                  online
+                </span>
+              </div>
+              <p class="w-full text-sm font-medium text-white mt-1">
+                <?= $sales['additional_sentence']; ?>
+              </p>
+            </div>
+
+          </a>
+        <?php endforeach; ?>
+      </div>
+    </section>
+    <!-- CTA SECTION -->
+
+    <!-- FOOTER -->
+    <footer class="bg-black py-5 flex justify-center items-center">
+      <p class="text-sm text-white">Vicentra © 2025 Created By Vicentra Dev.</p>
+    </footer>
+    <!-- FOOTER -->
+  </div>
+
+  <script defer type="module" src="./assets/js/app.js"></script>
+</body>
+
+</html>
